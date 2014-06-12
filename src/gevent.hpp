03aa725a3c4f5872b48a1be160eddfb4cabc61fa
Giuseppe Lipari <giuseppe.lipari@lsv.ens-cachan.fr>
2014-06-12 12:41:14 +0200

    Hidden the copy constructor for entity. Removed the register_handler() for geven, and added a new constructor

--- src/gevent.hpp
+++ src/gevent.hpp
@@ -26,96 +26,97 @@
 #include <event.hpp>
 
 namespace MetaSim {
-  /**
-     \ingroup metasim_ee
-
-     This template class is used for simplifying the writing of a
-     system model. Most of the times, the doit() method of an event
-     must only call a handler method of some class. Normally, you must
-     write a new class derived from event and override the virtual
-     doit() method. However, it is a lot of repetitive work! And when
-     there is a lot of repetitive work to do, template is the
-     solution!
-
-     The GEvent class takes a class derived from Entity as a template
-     argument. Then, template function register_handler() must be
-     called passing the pointer to the handler before using this
-     event.
-
-     An example of usage is found in example/eth/netinterface.hpp:
-
-     \dontinclude netinterface.hpp
-     \skipline class EthernetInterface
-     \until EthernetInterface(
-
-     The _trans_evt event object is of type GEvent<X>: there is not
-     need of defining a special class derived from event. The
-     constructor for EthernetInterface must invoke the
-     register_handler() function:
-
-     \dontinclude netinterface.cpp
-     \skipline EthernetInterface::EthernetInterface
-     \until register_handler
-
-     Handler onTransmit() is called whenever the event is triggered.
-  */
-  template<class X>
-  class GEvent : public Event {
-  public:
-    typedef void (X::* Pmemfun)(Event *);
-
-    X *_obj;
-    Pmemfun _fun;
-
     /**
-       Constructore for GEvent, simply initializes to null all
-       internal methods. It is possible to specify a priority.
+       \ingroup metasim_ee
+
+       This template class is used for simplifying the writing of a
+       system model. Most of the times, the doit() method of an event
+       must only call a handler method of some class. Normally, you must
+       write a new class derived from event and override the virtual
+       doit() method. However, it is a lot of repetitive work! And when
+       there is a lot of repetitive work to do, template is the
+       solution!
+
+       The GEvent class takes a class derived from Entity as a template
+       argument. Then, template function register_handler() must be
+       called passing the pointer to the handler before using this
+       event.
+
+       An example of usage is found in example/eth/netinterface.hpp:
+
+       \dontinclude netinterface.hpp
+       \skipline class EthernetInterface
+       \until EthernetInterface(
+
+       The _trans_evt event object is of type GEvent<X>: there is not
+       need of defining a special class derived from event. The
+       constructor for EthernetInterface must invoke the
+       register_handler() function:
+
+       \dontinclude netinterface.cpp
+       \skipline EthernetInterface::EthernetInterface
+       \until register_handler
+
+       Handler onTransmit() is called whenever the event is triggered.
+    */
+    template<class X>
+    class GEvent : public Event {
+	typedef void (X::* Pmemfun)(Event *);
+
+	X *_obj;
+	Pmemfun _fun;
+
+    public:
+	/**
+	   Constructore for GEvent, simply initializes to null all
+	   internal methods. It is possible to specify a priority.
        
-       @param p Priority of this event
-     */
-    GEvent(int p = Event::_DEFAULT_PRIORITY) : 
-      Event(p), _obj(NULL), _fun(NULL)
-    {}
+	   @param p Priority of this event
+	*/
+	GEvent(int p = Event::_DEFAULT_PRIORITY) : 
+	    Event(p), _obj(NULL), _fun(NULL)
+	    {}
+
+	/**
+	   Copy constructor. Useful when creating many events of the same
+	   type for the same \b object. (Remember: all these events point
+	   to the same object!)
+	*/
+	GEvent(GEvent<X> &e) : Event(e) { 
+	    _obj = e._obj; _fun = e._fun; 
+	}
+
+	GEvent(X *obj, typename GEvent<X>::Pmemfun fun, int p = Event::_DEFAULT_PRIORITY) :
+	    Event(p), _obj(obj), _fun(fun) {}
+
+	/**
+	   Simply calls the specified handler on the specified object.
+
+	   \sa register_handler
+	*/
+	virtual void doit() { 
+	    if ((_obj != NULL) && (_fun != NULL))
+		(_obj->*_fun)(this);
+	}
+    };
 
     /**
-       Copy constructor. Useful when creating many events of the same
-       type for the same \b object. (Remember: all these events point
-       to the same object!)
-     */
-    GEvent(GEvent<X> &e) : Event(e) 
-    { 
-      _obj = e._obj; _fun = e._fun; 
-    }
+       \ingroup metasim_ee
 
-    /**
-       Simply calls the specified handler on the specified object.
-
-       \sa register_handler
-     */
-    virtual void doit() 
-    { 
-      if ((_obj != NULL) && (_fun != NULL))
-	(_obj->*_fun)(this);
-    }
-  };
-
-  /**
-     \ingroup metasim_ee
-
-     This function is used to specify that a certain event of type
-     GEvent<X> must refer to object obj and call its method fun.
+       This function is used to specify that a certain event of type
+       GEvent<X> must refer to object obj and call its method fun.
      
-     \param evt the event object
-     \param obj the entity obj 
-     \param fun the method of obj to be called
-     \sa GEvent#doit
-   */
-  template<class X>
-  void register_handler(GEvent<X> &evt, X *obj,
-			typename GEvent<X>::Pmemfun fun) 
-  { 
-    evt._obj = obj; evt._fun = fun; 
-  }
+       \param evt the event object
+       \param obj the entity obj 
+       \param fun the method of obj to be called
+       \sa GEvent#doit
+    */
+    // template<class X>
+    // void register_handler(GEvent<X> &evt, X *obj,
+    // 			  typename GEvent<X>::Pmemfun fun) 
+    // { 
+    // 	evt._obj = obj; evt._fun = fun; 
+    // }
 }  
 
 #endif
