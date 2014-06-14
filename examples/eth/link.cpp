a35a924aa92ab65e9bc21e0ce154ce91fbf8ea78
Giuseppe Lipari <giuseppe.lipari@lsv.ens-cachan.fr>
2014-06-14 10:33:22 +0200

    Other modifications in the examples

--- examples/eth/link.cpp
+++ examples/eth/link.cpp
@@ -15,19 +15,19 @@ Link::~Link()
 }
 
 EthernetLink::EthernetLink(const char *name) 
-        : Link(name), _interfaces(), _contending(), 
-          _isBusy(false),
-          _isContending(false),
-          _isCollision(false),
-          _contention_period(10),
-          _message(0),
-          _end_contention_evt(),
-          _collision_evt(),
-          _end_transmission_evt()
+    : Link(name), _interfaces(), _contending(), 
+      _isBusy(false),
+      _isContending(false),
+      _isCollision(false),
+      _contention_period(10),
+      _message(0),
+      _end_contention_evt(this, &EthernetLink::onEndContention),
+      _collision_evt(this, &EthernetLink::onCollision),
+      _end_transmission_evt(this, &EthernetLink::onEndTransmission)
 {
-        register_handler(_end_contention_evt, this, &EthernetLink::onEndContention);
-        register_handler(_collision_evt, this, &EthernetLink::onCollision);
-        register_handler(_end_transmission_evt, this, &EthernetLink::onEndTransmission);
+    //register_handler(_end_contention_evt, this, &EthernetLink::onEndContention);
+    //register_handler(_collision_evt, this, &EthernetLink::onCollision);
+    //register_handler(_end_transmission_evt, this, &EthernetLink::onEndTransmission);
 }
 
 EthernetLink::~EthernetLink()
@@ -36,11 +36,11 @@ EthernetLink::~EthernetLink()
 
 void EthernetLink::newRun()
 {
-        _contending.clear();
-        _isBusy = false;
-        _isContending = false;
-        _isCollision = false;
-        _message = 0;
+    _contending.clear();
+    _isBusy = false;
+    _isContending = false;
+    _isCollision = false;
+    _message = 0;
 }
  
 void EthernetLink::endRun()
@@ -49,7 +49,7 @@ void EthernetLink::endRun()
 
 bool EthernetLink::isBusy()
 {
-        return _isBusy;
+    return _isBusy;
 }
 
 void EthernetLink::send(Message *m)
@@ -58,21 +58,21 @@ void EthernetLink::send(Message *m)
 
 void EthernetLink::contend(EthernetInterface *eth, Message *m)
 {
-        DBGENTER(_ETHLINK_DBG);
+    DBGENTER(_ETHLINK_DBG);
  
-        if (_isContending) {
-                _end_contention_evt.drop();
-                if (!_isCollision) {
-                        _isCollision = true;
-                        _collision_evt.post(SIMUL.getTime() + 3);
-                }
-        }
-        else {
-                _isContending = true;
-                _message = m;
-                _end_contention_evt.post(SIMUL.getTime() + _contention_period);
-        }
-        _contending.push_back(eth);
+    if (_isContending) {
+	_end_contention_evt.drop();
+	if (!_isCollision) {
+	    _isCollision = true;
+	    _collision_evt.post(SIMUL.getTime() + 3);
+	}
+    }
+    else {
+	_isContending = true;
+	_message = m;
+	_end_contention_evt.post(SIMUL.getTime() + _contention_period);
+    }
+    _contending.push_back(eth);
 
         
 }
@@ -92,34 +92,30 @@ void EthernetLink::onEndContention(Event *e)
 
 void EthernetLink::onCollision(Event *e)
 {
-        DBGENTER(_ETHLINK_DBG);
+    DBGENTER(_ETHLINK_DBG);
 
-        _isContending = false;
-        _isCollision = false;
-        _isBusy = false;
-        while (!_contending.empty()) {
-                _contending.back()->onCollision();
-                _contending.pop_back();
-        }
-        _message = 0;
-
-        
+    _isContending = false;
+    _isCollision = false;
+    _isBusy = false;
+    while (!_contending.empty()) {
+	_contending.back()->onCollision();
+	_contending.pop_back();
+    }
+    _message = 0;        
 }
 
 void EthernetLink::onEndTransmission(Event *e)
 {
-        Message *m = _message;
+    Message *m = _message;
 
-        DBGENTER(_ETHLINK_DBG);
+    DBGENTER(_ETHLINK_DBG);
 
-        NetInterface *dst = _message->getDestInterface();
-        NetInterface *src = _message->getSourceInterface();
+    NetInterface *dst = _message->getDestInterface();
+    NetInterface *src = _message->getSourceInterface();
 
-        _isBusy = false;
-        _message = 0;
+    _isBusy = false;
+    _message = 0;
 
-        dst->onMessageReceived(m);
-        src->onMessageSent(m);
-
-        
+    dst->onMessageReceived(m);
+    src->onMessageSent(m);        
 }
--- examples/eth/node.cpp
+++ examples/eth/node.cpp
@@ -11,15 +11,17 @@ using namespace MetaSim;
 
 Node::Node(string const & name) 
         : Entity(name), _net_interf(0), _interval(0),
-          _nodes(), _recv_evt(), _send_evt()
+          _nodes(), 
+	  _recv_evt(this, &Node::onReceive), 
+	  _send_evt(this, &Node::onSend)
 {
-        register_handler(_recv_evt, this, &Node::onReceive);
-        register_handler(_send_evt, this, &Node::onSend);
+    // register_handler(_recv_evt, this, &Node::onReceive);
+    // register_handler(_send_evt, this, &Node::onSend);
 }
 
 void Node::newRun()
 {
-        _send_evt.post((int)_interval->get());
+    _send_evt.post((int)_interval->get());
 }
 
 void Node::endRun()
@@ -29,53 +31,51 @@ void Node::endRun()
 
 NetInterface *Node::getNetInterface()
 {
-        return _net_interf;
+    return _net_interf;
 }
 
 void Node::setNetInterface(NetInterface &n)
 {
-        _net_interf = &n;
+    _net_interf = &n;
 }
 
 void Node::onReceive(Event *e)
 {
-        // simply, record the fact that the message has succesfully been 
-        // received.
- 
-        DBGTAG(_NODE_DBG, getName() + "::onReceive()");
+    // simply, record the fact that the message has succesfully been 
+    // received.
+    
+    DBGTAG(_NODE_DBG, getName() + "::onReceive()");
 }
 
 void Node::onMessageReceived(Message *m)
 {
-        // simply, record the fact that the message has succesfully been 
-        // received.
- 
-        DBGTAG(_NODE_DBG, getName() + "::onMessageReceived()");
+    // simply, record the fact that the message has succesfully been 
+    // received.
+    
+    DBGTAG(_NODE_DBG, getName() + "::onMessageReceived()");
 }
 
 void Node::setInterval(auto_ptr<RandomVar> i)
 {
-        _interval = i;
+    _interval = i;
 }
 
 void Node::addDestNode(Node &n)
 {
-        _nodes.push_back(&n);
+    _nodes.push_back(&n);
 }
 
 void Node::onSend(Event *e)
 {
-        UniformVar len(100,1500);
-        UniformVar n(0, _nodes.size());
-        int i = (int)n.get();
-
-        DBGENTER(_NODE_DBG);
-
-        DBGPRINT("dest node = " << _nodes[i]->getName());
-        // creates a new message and send it!! 
-        Message *m = new Message((int)len.get(), this, _nodes[i]);
-        _net_interf->send(m);
-        _send_evt.post(SIMUL.getTime() + (Tick)_interval->get());
-
-        
+    UniformVar len(100,1500);
+    UniformVar n(0, _nodes.size());
+    int i = (int)n.get();
+    
+    DBGENTER(_NODE_DBG);
+    
+    DBGPRINT("dest node = " << _nodes[i]->getName());
+    // creates a new message and send it!! 
+    Message *m = new Message((int)len.get(), this, _nodes[i]);
+    _net_interf->send(m);
+    _send_evt.post(SIMUL.getTime() + (Tick)_interval->get());    
 }
