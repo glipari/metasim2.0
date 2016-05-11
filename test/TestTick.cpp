912f7329d69bcafa7a40727bbb786ab5eeefe583
Alessio Balsini <a.balsini@sssup.it>
2016-05-11 11:02:45 +0200

    Added test for checking overload conditions

--- test/TestTick.cpp
+++ test/TestTick.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <tick.hpp>
+#include <stdint.h>
+#include <climits>
 
 #include "catch.hpp"
 
@@ -33,6 +35,34 @@ TEST_CASE("TestTick2", "testOperations")
     REQUIRE(a++ == 0);
     REQUIRE(++a == 2);
     REQUIRE((d/400) == 1);
+}
+
+TEST_CASE("TestTick3", "testOverflow")
+{
+    Tick sc_m = SCHAR_MIN;
+    Tick sc_M = SCHAR_MAX;
+    REQUIRE(sc_m == SCHAR_MIN);
+    REQUIRE(sc_M == SCHAR_MAX);
+
+    Tick ss_m = SHRT_MIN;
+    Tick ss_M = SHRT_MAX;
+    REQUIRE(ss_m == SHRT_MIN);
+    REQUIRE(ss_M == SHRT_MAX);
+
+    Tick i_m = INT_MIN;
+    Tick i_M = INT_MAX;
+    REQUIRE(i_m == INT_MIN);
+    REQUIRE(i_M == INT_MAX);
+
+    Tick l_m = LONG_MIN;
+    Tick l_M = LONG_MAX;
+    REQUIRE(l_m == LONG_MIN);
+    REQUIRE(l_M == LONG_MAX);
+
+    Tick ll_m = LONG_MIN;
+    Tick ll_M = LONG_MAX;
+    REQUIRE(ll_m == LONG_MIN);
+    REQUIRE(ll_M == LONG_MAX);
 }
 
 
